261a174506d1d1e9a6d78cb780bf54aeb1ec0ccf
d0uglasmonteiro <141074432+d0uglasmonteiro@users.noreply.github.com>
2023-09-19 13:07:36 -0300

    Update livros.js

--- livros.js
+++ livros.js
@@ -1,7 +1,41 @@
-import {obterLivros,Incluir,Excluir} from 'livro-dao.js'
-var express=require('express')
-router=express.Router
-router.get('/', function router (req, res){res.send('livros'obterLivros)})
-router.post('/', function router (req, res){req.send('livro'incluir)})
+const express = require('express');
+const router = express.Router();
+const LivroDAO = require('../modelo/livro-dao'); 
 
-export default router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const livros = await LivroDAO.obterLivros();
+    res.json(livros);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.post('/', async (req, res, next) => {
+  const novoLivro = req.body; // Os dados do novo livro devem ser enviados no corpo da requisição
+  try {
+    const livroInserido = await LivroDAO.incluir(novoLivro);
+    res.status(201).json({ message: 'Livro adicionado com sucesso', livro: livroInserido });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.delete('/:id', async (req, res, next) => {
+  const codigoLivro = req.params.id;
+  try {
+    const resultado = await LivroDAO.excluir(codigoLivro);
+    if (resultado.deletedCount === 1) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Livro não encontrado' });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

28744bb0c7559316dce71ea594d6155050f79a09
d0uglasmonteiro <141074432+d0uglasmonteiro@users.noreply.github.com>
2023-09-19 12:59:13 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,5 +1,11 @@
 const { default: mongoose } = require("mongoose");
 
-var banco=require("mongoose")
-var options={useUnifiedTopology=true,useNewUrlParser=true}
-export default banco
+const options={useUnifiedTopology=true,useNewUrlParser=true};
+
+const mongoURL='mongodb://localhost:27017'
+
+mongoose.connect(mongoURL, options)
+.then(()=>console.log('conexao com o mongoDB estabelecida.'))
+.catch(erro=> console.error('Erro na conexão com o mongoDB:',erro));
+
+module.exports=mongoose;
